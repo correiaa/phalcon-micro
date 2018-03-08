5686adc469ce366904d02eeb521fb2242a943c59
nilnice <nilnice@126.com>
2018-03-08 13:48:31 +0800

    : ) Update Response class.

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -44,9 +44,10 @@ class Response extends \Phalcon\Http\Response
             $method = $request->getMethod();
             $uri = $request->getURI();
             $devInfo = [
+                'code'          => $e->getCode(),
+                'message'       => $e->getMessage(),
                 'file'          => $e->getFile(),
                 'line'          => $e->getLine(),
-                'message'       => $msg,
                 'request'       => $method . ' ' . $uri,
                 'previous'      => $e->getPrevious(),
                 'trace'         => $e->getTrace(),
